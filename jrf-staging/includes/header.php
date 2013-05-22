57441826cb811aafadc53961417e47177f667a05
Alejandro Veloz <aveloz.intern@beatsbydre.com>
2013-05-22 06:10:57 -0700

    Moved future code (beyond JRF) to new file

--- jrf-staging/includes/header.php
+++ jrf-staging/includes/header.php
@@ -2,5 +2,7 @@
 
 	<a href="./index.php"><img alt="" src="http://i47.tinypic.com/345cvwy.png" style="max-width: 100%; height: auto;"></a>
 	<!--img class="elbow" alt="" src="./images/header-elbow.png" style="position:absolute;max-width: 100%; height: auto;">-->
+	<!--<img src="./images/header-elbow.png" style="position:relative; top:100%;"> -->
+	
 
 </div> <!-- end #header -->
--- jrf-staging/lineup.php
+++ jrf-staging/lineup.php
@@ -96,15 +96,7 @@
 				</div>
 				
 				
-				<!--
-				<div class="grid-img">
-					<a href="./artist.php">
-						<img src="http://www.paradigmagency.com/Asset/s/a/santigold-main--photo-credit-sean-thomas-extralarge_1327359655725-medium.jpg"
-						<div class="grid-img-overlay"
-							<div class="overlay-text"Santigold</div
-						</div
-					</a
-				</div -->
+				
 				
 			</div> <!-- end #content -->
 			
