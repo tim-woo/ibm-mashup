9cc49a0b73e7c6d35ebb5ae11d363aeeec7bdf8e
Jay Singh <beatsbydre1@Jays-MacBook-Pro.local>
2013-05-22 07:39:38 -0700

    Santigold and Common Artist Pages using artistfunctions.php

--- jrf-staging/artist.php
+++ jrf-staging/artist.php
@@ -40,43 +40,12 @@
 			<?php include('./includes/nav.php'); ?>
 			<?php include('./includes/nav-side.php'); ?>
 
-			<?php include('./includes/artist_functions.php'); ?>
+			
 			
 			<!-- Variable Content Goes Here -->
-			<div id="content">
-			
-			<h1> <?php echo $_GET['id']; ?> </h1> <!-- Just a test, remove eventually -->
-			
-				<img src="http://www.paradigmagency.com/Asset/s/a/santigold-main--photo-credit-sean-thomas-extralarge_1327359655725-medium.jpg">
-				<div class="bio-container">
-					Santigold is Santi White: an artist whose perseverance relies on invention, a champion who survives off her own skill and faith. She is a major muse watched by the inspired world, an in categorical performer who collapses time and genre with one hand guided by tradition, while the other hand carves out a shining future. Santigold is neither calm nor mayhem, but from her lungs burst every color in between. 
-
-After four years of hide and seek in which blogs blew up at the sudden release of any track with her name attached to it, Santigold returns in a moment of global aggression and vulnerability. Honing in on the hyper-media cult of personality, in her unmistakable voice she asks the listener: Into what fantasy do you hurl yourself as you gaze into the glow of your machine? The answer to this question is central to what drives "Master of My Make-Believe," her latest work.
-				</div>
-				
-				<h1 class="mashup-type"> YOUTUBE </h1>
-				<div class="video-container">
-					<iframe src="http://www.youtube.com/embed/mIMMZQJ1H6E" frameborder="0" allowfullscreen></iframe>
-				</div>
-				
-				<h1 class="mashup-type"> SOUNDCLOUD </h1>
-				<div class="audio-container">
-				<iframe width="100%" height="166" scrolling="no" frameborder="no" src="https://w.soundcloud.com/player/?url=http%3A%2F%2Fapi.soundcloud.com%2Ftracks%2F47836153"></iframe>
-				</div>
-				<div class="audio-container">
-				<iframe width="100%" height="166" scrolling="no" frameborder="no" src="https://w.soundcloud.com/player/?url=http%3A%2F%2Fapi.soundcloud.com%2Ftracks%2F43420259"></iframe>
-				</div>
-				
-				<h1 class="mashup-type"> RELATED ARTISTS </h1>
+			<?php include('./includes/artist_functions.php'); ?>
 
 
-
-				<h1 class="mashup-type"> SOCIAL MEDIA </h1>	
-
-
-				
-								
-			</div> <!-- end #content -->
 			
 			<?php include('./includes/footer.php'); ?>
 			
--- jrf-staging/includes/artist_functions.php
+++ jrf-staging/includes/artist_functions.php
@@ -1,31 +1,110 @@
 <!-- artist_functions.php -->
-<html>
 
-<?php
-switch ($id)
+<?php 
+			if ($_GET['id'] == 'santigold')
+			{ ?>
+
+
+				<div id="content">
+				
+					<h1> <?php echo 'Santigold'; ?> </h1> <!-- Just a test, remove eventually -->
+			
+						<img src="http://www.paradigmagency.com/Asset/s/a/santigold-main--photo-credit-sean-thomas-extralarge_1327359655725-medium.jpg">
+						<div class="bio-container">
+						Santigold is Santi White: an artist whose perseverance relies on invention, a champion who survives off her own skill and faith. She is a major muse watched by the inspired world, an in categorical performer who collapses time and genre with one hand guided by tradition, while the other hand carves out a shining future. Santigold is neither calm nor mayhem, but from her lungs burst every color in between. 
+
+						After four years of hide and seek in which blogs blew up at the sudden release of any track with her name attached to it, Santigold returns in a moment of global aggression and vulnerability. Honing in on the hyper-media cult of personality, in her unmistakable voice she asks the listener: Into what fantasy do you hurl yourself as you gaze into the glow of your machine? The answer to this question is central to what drives "Master of My Make-Believe," her latest work.
+						</div>
+				
+					<h1 class="mashup-type"> YOUTUBE </h1>
+						<div class="video-container">
+							<iframe src="http://www.youtube.com/embed/mIMMZQJ1H6E" frameborder="0" allowfullscreen></iframe>
+						</div>
+					
+					<h1 class="mashup-type"> SOUNDCLOUD </h1>
+						<div class="audio-container">
+							<iframe width="100%" height="166" scrolling="no" frameborder="no" src="https://w.soundcloud.com/player/?url=http%3A%2F%2Fapi.soundcloud.com%2Ftracks%2F47836153"></iframe>
+						</div>
+						<div class="audio-container">
+							<iframe width="100%" height="166" scrolling="no" frameborder="no" src="https://w.soundcloud.com/player/?url=http%3A%2F%2Fapi.soundcloud.com%2Ftracks%2F43420259"></iframe>
+						</div>
+					
+					<h1 class="mashup-type"> RELATED ARTISTS </h1>
+
+
+					<h1 class="mashup-type"> SOCIAL MEDIA </h1>	
+						
+			</div> <!-- end #content -->
+
+	<?php 	}
+			else if ($_GET['id'] == 'common')
+			{ ?>
+
+
+				<div id="content">
+				
+					<h1> <?php echo 'Common'; ?> </h1> <!-- Just a test, remove eventually -->
+			
+						<img src="http://cdn.pigeonsandplanes.com/wp-content/uploads/2012/01/commonlrg.jpg">
+						<div class="bio-container">
+						Since his debut in 1992 with Can I Borrow a Dollar?, COMMON has charted a constant course toward self-expansion and reinvention. He’s gone from a battle rhymer flipping clever punchlines to a conscious rapper exploring themes of love and light to the complex, multi-talented artist that he is today. Through it all, he’s stayed true to his family, to his friends, and to his South Side Chicago roots.
+
+						</div>
+				
+					<h1 class="mashup-type"> YOUTUBE </h1>
+						<div class="video-container">
+							<iframe src="http://www.youtube.com/embed/S7B2VgRShew" frameborder="0" allowfullscreen></iframe>
+						</div>
+					
+					<h1 class="mashup-type"> SOUNDCLOUD </h1>
+						<div class="audio-container">
+							<iframe width="100%" height="166" scrolling="no" frameborder="no" src="https://w.soundcloud.com/player/?url=http%3A%2F%2Fapi.soundcloud.com%2Ftracks%2F86558346&amp;color=ff6600&amp;auto_play=false&amp;show_artwork=true"></iframe>
+						</div>
+						<div class="audio-container">
+							<iframe width="100%" height="166" scrolling="no" frameborder="no" src="https://w.soundcloud.com/player/?url=http%3A%2F%2Fapi.soundcloud.com%2Ftracks%2F43420259"></iframe>
+						</div>
+					
+					<h1 class="mashup-type"> RELATED ARTISTS </h1>
+
+
+					<h1 class="mashup-type"> SOCIAL MEDIA </h1>	
+						
+			</div> <!-- end #content -->
+
+	<?php 	}
+
+
+
+/* function getArtistURL($_GET['id'])
 {
-	case "santigold":
+	switch ($_GET['id'])
+	{ 
+		case "santigold": ?>
+			<h1> <?php echo $_GET['id']; ?> </h1>;
+
+
+<?php		break;
+
+		case 'santigold':
+
+		case "common":
+
+		case "grouch":
+
+		case "jhene":
+
+		case "ryan":
+
+		case "ziggy":
+
+		case "barrington":
+
+		case "vegas":
+
+		case "marcia":
+
 	
-
-	case "common":
-
-	case "grouch":
-
-	case "jhene":
-
-	case "ryan":
-
-	case "ziggy":
-
-	case "barrington":
-
-	case "vegas":
-
-	case "marcia":
-
-	
-}
+	}
+} */
 
 ?>
-
-</html>
